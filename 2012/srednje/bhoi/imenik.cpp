c88a9a35a19e3d3838b654c55e0545f7d6ae2e54
Muamer Parić <muamer.paric@infobip.com>
2025-07-05 13:50:04 +0200

    Uređivanje i preimenovanje

---
+++ 2012/srednje/bhoi/imenik.cpp
@@ -0,0 +1,157 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#define mp make_pair
+#define xx first
+#define yy second
+#define pb push_back
+using namespace std;
+const int Poc=100000;
+struct node
+{
+    int t[11];
+    vector<int> prlz;
+    node(){memset(t,-1,sizeof t);prlz.clear();}
+};
+struct trie
+{
+    vector<node> x;
+    vector<int> tmp;
+    int root,T;
+    trie(){x.resize(Poc);root=0;T=0;}
+    int add(int tr,char vl,int inx)
+    {
+//        if(!x[tr].prlz.size())x[tr].prlz.pb(inx);
+//        if(x[tr].prlz.back()!=inx)x[tr].prlz.pb(inx);
+        if(x[tr].t[vl]!=-1)return x[tr].t[vl];
+        else
+        {
+            if(T+50>x.size())x.resize(x.size()+500);
+            return x[tr].t[vl]=++T;
+        }
+    }
+    void dfs(int tr)
+    {
+        for(int i=0;i<10;i++)
+        {
+            if(x[tr].t[i]!=-1)
+                dfs(x[tr].t[i]);
+        }
+        for(int i=0;i<x[tr].prlz.size();i++)tmp.pb(x[tr].prlz[i]);
+    }
+    void prtz(string a,vector<int> &z)
+    {
+        int I=0,tr=root;
+        //printf("%d\n",x.size());
+
+        while(I<a.size() && tr!=-1)
+        {
+            //printf("%d\n",tr);
+            tr=x[tr].t[a[I++]-'0'];
+        }
+        if(tr!=-1)
+        {
+            tmp.clear();
+            dfs(tr);
+            sort(tmp.begin(),tmp.end());
+            tmp.resize(unique(tmp.begin(),tmp.end())-tmp.begin());
+            z=tmp;
+        }
+        else z=vector<int>();
+    }
+}drv;
+int ubr(char p)
+{
+    p=tolower(p);
+    if(isdigit(p))return p-'0';
+    if('a'<=p && p<='c')return 2;
+    if('d'<=p && p<='f')return 3;
+    if('g'<=p && p<='i')return 4;
+    if('j'<=p && p<='l')return 5;
+    if('m'<=p && p<='o')return 6;
+    if('p'<=p && p<='s')return 7;
+    if('t'<=p && p<='v')return 8;
+    if('w'<=p && p<='z')return 9;
+    return 1;
+}
+struct ime
+{
+    char komp[60];
+    ime(){}
+    ime(string &a,int inx)
+    {
+		while(a[a.size()-1]==' ')a.resize(a.size()-1);
+		memset(komp,0,sizeof komp);
+        for(int i=0;a[i];i++)komp[i]=a[i];
+        a+=' ';
+        int I=12,tr;
+        while(I<a.size())
+        {
+            tr=drv.root;
+			int inii=I;
+            while(a[I]!=' ')
+            {
+                if(I-inii<11)
+                    tr=drv.add(tr,ubr(a[I]),inx);
+                //komp[I]=ubr(a[I])+'0';
+                I++;
+            }
+            if(!drv.x[tr].prlz.size())drv.x[tr].prlz.pb(inx);
+            if(drv.x[tr].prlz.back()!=inx)drv.x[tr].prlz.pb(inx);
+            drv.add(tr,1,inx);
+            I++;
+        }
+    }
+//    void isp(){puts(komp.c_str());}
+};
+bool cmp(const string &x,const string &y)
+{
+    for(int i=12;i<min(x.size(),y.size());i++)
+    {
+        if(x[i]!=y[i])return x[i]<y[i];
+    }
+    return x.size()<=y.size();
+}
+char s[500];
+vector<ime> svi;
+vector<string> tmp;
+vector<int> z;
+int n,q;
+int main()
+{
+    //freopen("imenik8.in","r",stdin);
+    //freopen("out.txt","w",stdout);
+    drv=trie();
+	scanf("%d %d",&n,&q);
+	svi.resize(n);tmp.resize(n);
+	for(int i=0;i<n;i++)
+	{
+	    scanf(" ");gets(s);
+	    tmp[i]=s;
+	}
+	stable_sort(tmp.begin(),tmp.end(),&cmp);
+	for(int i=0;i<n;i++)svi[i]=ime(tmp[i],i);
+	while(q--)
+    {
+        scanf(" ");gets(s);
+        z.clear();
+        drv.prtz(s,z);
+        printf("%s:\n",s);
+        for(int i=0;i<z.size();i++)
+        {
+            puts(svi[z[i]].komp);
+            //cout<<svi[z[i]].komp<<endl;
+            //puts("A");
+        }
+    }
+    return 0;
+}
---
+++ 2012/srednje/bhoi/kopiranje.cpp
@@ -0,0 +1,88 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#define mp make_pair
+#define xx first
+#define yy second
+#define pb push_back
+using namespace std;
+typedef long long lint;
+int n,k;
+vector<int> a;
+bool moze(lint x)
+{
+    lint trs=0;
+    int f=0;
+    for(int i=n-1;i>=0;i--)
+    {
+        if(trs+a[i]>x){trs=a[i];f++;}
+        else trs+=(lint)a[i];
+    }
+    return f<k;
+}
+vector<int> f;
+void isp(lint x)
+{
+    lint trs=0;
+    f.reserve(k+1);
+    for(int i=n-1;i>=0;i--)
+    {
+        if(trs+a[i]>x){trs=a[i];f.pb(i);}
+        else trs+=(lint)a[i];
+    }
+    int I=0;
+    reverse(f.begin(),f.end());
+    int ubc=-1,dos=f.size();
+    for(int i=0;i<k-dos-1;i++){while(binary_search(f.begin(),f.begin()+dos,++ubc));f.pb(ubc);}
+    sort(f.begin(),f.end());
+    for(int i=0;i<n;i++)
+    {
+        printf("%d",a[i]);
+        if(I<f.size() && f[I]==i){putchar(' ');putchar('/');putchar(' ');I++;}
+        else if(i!=n-1)putchar(' ');
+    }
+   // printf(" %d %d\n",f.size(),k);
+    puts("");
+}
+char c;
+inline void fast_input(int &x)
+{
+    while((c=getchar())<'0');
+    x=c-'0';
+    while((c=getchar())>='0')x=x*10+c-'0';
+}
+int main()
+{
+    //freopen("kopiranje10.in","r",stdin);
+	scanf("%d %d",&n,&k);a.resize(n);
+	lint low=1<<29,high=0,mid=0;
+	for(int i=0;i<n;i++)
+    {
+        //scanf("%d",&a[i]);
+        a[i]=0;
+        fast_input(a[i]);
+        high+=(lint)a[i];
+        low=min(low,(lint)a[i]-1);
+    }
+    low=max(high/k+1,low);
+    while(low<high)
+    {
+        if(low==high-1){if(moze(low))high=low;else low=high;break;}
+        mid=(low+high)/2;
+        if(moze(mid))high=mid;
+        else low=mid;
+    }
+    //printf("%d\n",low);
+    isp(low);
+    return 0;
+
+}
